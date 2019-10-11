b6d3d44d8d0abf5647875d8add4c0fa95bd0d035
Marise St-Pierre <mstpierre@myca.com>
2019-10-11 07:53:27 -0400

    created model

---
+++ js/models/todo.js
@@ -0,0 +1,13 @@
+const app = app || {};
+
+app.Todo = Backbone.Model.extend({
+	defaults: {
+		title: "",
+		completed: false
+	},
+	toggle: function() {
+		this.save({
+			completed: !this.get("completed")
+		});
+	}
+});
